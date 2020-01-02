783409ab7cc280e96b67cd804fdfaf2202da8502
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2020-01-01 23:18:09 -0500

    making rest endpoint for state machine

---
+++ StateMachine/Controllers/loginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+
+namespace StateMachine.Controllers
+{
+    
+    public class loginController : Controller
+    {
+        // login?username=testuser&password=password
+        public JsonResult index(string username, string password)
+        {
+            if (username == null || password == null)
+            {
+                return Json("username or password not provided");
+            }
+            else
+            {
+                Models.User user = new Models.User()
+                {
+                    username = username,
+                    password = password
+                };
+
+                //var json = JsonConvert.SerializeObject(user);
+
+                return Json(user);
+
+            }
+        }
+    }
+}
---
+++ StateMachine/Services/JWT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+
+namespace StateMachine.Services
+{
+    public class JWT
+    {
+        /****
+        public static Task<ClaimsIdentity> CreateClaimIdAsync(Models.User user)
+        {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.username.ToString()));
+
+           
+
+        }
+         * ****/
+
+    }
+}
